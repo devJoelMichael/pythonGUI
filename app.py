5d5dcfd66958579e0d7108133bb3b8106408d3dc
Joel Poirier <devjoelmichael@Joels-Mac-mini.local>
2021-04-10 16:55:13 -0700

    working applications into GUI

--- app.py
+++ app.py
@@ -3,6 +3,21 @@ from tkinter import filedialog, Text
 import os
 
 root = tk.Tk()
+apps = []
+
+
+def addApp():
+
+    for widget in frame.winfo_children():
+
+    filename = filedialog.askopenfilename(
+        initialdir="/", title="Select File", filetypes=(("executables", "*.app"), ("all files", "*.*")))
+    apps.append(filename)
+    print(filename)
+    for app in apps:
+        label = tk.Label(frame, text=app, bg="gray")
+        label.pack()
+
 
 canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
 canvas.pack()
@@ -11,7 +26,7 @@ frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
 openFile = tk.Button(root, text="Open File", padx=10,
-                     pady=5, fg="white", bg="red")
+                     pady=5, fg="white", bg="red", command=addApp)
 openFile.pack()
 
 runApps = tk.Button(root, text="Run Apps", padx=10,
