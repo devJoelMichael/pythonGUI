d12d365d04285541aae83d255d7daf352db8f4bd
Joel Poirier <devjoelmichael@Joels-Mac-mini.local>
2021-04-10 16:29:54 -0700

    int commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
+canvas.pack()
+
+frame = tk.Frame(root, bg="")
+
+root.mainloop()
